79134dd60b725e3bacd8ca64afdc6c6edae6de7b
Joyanta Kumar Sarker <35419547+joyantabd@users.noreply.github.com>
2019-09-15 17:32:22 +0600

    Joyanta_DataRange_Filter_Datatable

---
+++ app/Http/Controllers/DateRangeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
+
+class DateRangeController extends Controller
+{
+    function index(Request $request)
+    {
+        $users = Auth::user(1);
+        if(request()->ajax())
+        {
+            if(!empty($request->from_date))
+            {
+                $data = DB::table('table')
+                    ->whereBetween('order_date', array($request->from_date, $request->to_date))
+                    ->get();
+            }
+            else
+            {
+                $data = DB::table('table')
+                    ->get();
+            }
+            return datatables()->of($data)->make(true);
+        }
+        return view('daterange',compact('users'));
+    }
+}
+
+?>
